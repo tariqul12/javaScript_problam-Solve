cb8dece9791d0bb8c4938badd2e425390889fbcd
tariqul12 <mshimul591@gmail.com>
2022-02-18 21:10:28 +0600

    contitional

---
+++ Conditional.js
@@ -0,0 +1,15 @@
+let age= 17;
+// if(age===16||age===15||age===17){
+//     console.log("You are adult");
+// }
+let number=18;
+if(number<20){
+    console.log("number");
+}else{
+    console.log("not a number");
+}
+if(age==12&&age==14&&age==17){
+    console.log("Your are adult")
+}else{
+    console.log("You are not Adult");
+}
