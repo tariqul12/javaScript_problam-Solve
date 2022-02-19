ff899ea231156e5e949b5667409c29e08260a34b
tariqul12 <mshimul591@gmail.com>
2022-02-19 20:14:09 +0600

    clock done

---
+++ forLoop.js
@@ -0,0 +1,6 @@
+const friends=["Berlin","Denver","Rio","Alicia","Tokio","Nairobi"];
+for(let i=0;i<friends.length;i++){
+    const friend=friends[i];
+    console.log(i);
+    console.log(friend);
+}
